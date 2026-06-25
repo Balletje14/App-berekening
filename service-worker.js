e2a975a0b76eef7256cc462d037bb23207c6f9e5
Balletje14 <Ingvartuls@hotmail.com>
2026-06-25 11:02:35 +0200

    Refactor service worker caching logic
    Updated cache name and modified cached files list.

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,22 @@
-const CACHE_NAME = "bloemen-app-v1";
+const CACHE_NAME = "boeket-v2";
 
-const FILES_TO_CACHE = [
+const urlsToCache = [
   "./",
   "./index.html",
-  "./style.css",
   "./app.js",
-  "./manifest.json",
-  "./bloemen.json"
+  "./manifest.json"
 ];
 
-// Install: cache bestanden
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate: oude cache verwijderen
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch: offline werking
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
